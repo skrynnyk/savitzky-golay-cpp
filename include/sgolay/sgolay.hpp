0fda0f25384aaedc53cd97d20033b7954c059b3e
Alex Skrynnyk <skrynnykalex@gmail.com>
2023-01-02 22:46:44 -0800

    setting up project boilerplate

---
+++ include/sgolay/sgolay.hpp
@@ -0,0 +1,47 @@
+/**
+ * @file       sgolay.hpp
+ * 
+ * @author     Alex Skrynnyk (github.com/skrynnyk)
+ *
+ * @brief      This is a header-only implementation of the Slavitzky-Golay
+ *             Filter, backed by Gram polynomials for convolution weight
+ *             generation based on a paper by Peter A. Gorry "General
+ *             Least-Squares Smoothing and Differentiation by the Convolution
+ *             (Savitzky-Golay) Method".
+ *
+ * @copyright  Copyright 2023 Alex Skrynnyk. All rights reserved.
+ * 
+ * @license    This project is released under the MIT License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef SGOLAY_HPP
+#define SGOLAY_HPP
+
+/* Includes ------------------------------------------------------------------*/
+#include <array>
+#include <cstdint>
+
+
+namespace sgf {
+
+} /* namespace sgf */
+
+#endif /* SGOLAY_HPP */
